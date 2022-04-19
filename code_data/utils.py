31e68c1312ae0976db931c58abe3a343b68514f2
xishancx <44564213+xishancx@users.noreply.github.com>
2022-04-18 23:14:56 -0500

    Changed validation to testing

--- code_data/utils.py
+++ code_data/utils.py
@@ -10,8 +10,8 @@ def clip_gradient(model, clip_value):
 
 def plot_accuracies(train_acc, val_acc, model_name):
     plt.plot(train_acc, 'g', label='Training Accuracy')
-    plt.plot(val_acc, 'b', label='Validation Accuracy')
-    plt.title('Training and Validation Accuracy for ' + model_name)
+    plt.plot(val_acc, 'b', label='Testing Accuracy')
+    plt.title('Training and Testing Accuracy for ' + model_name)
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
